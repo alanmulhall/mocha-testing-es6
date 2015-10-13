67c80fe48d325ee2e9d1264537a13617fec9e999
Alan Mulhall <alan.mulhall@nfl.com>
2015-10-13 13:12:50 -0700

    Initial commit

---
+++ HelloWorld.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+let expect = chai.expect;
+
+describe("Hello", () => {
+	it("should be successful", () => {
+		expect(true).to.equal(true);
+	});
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+Basic Mocha Testing with ES6
+
+```
+npm install
+npm test
+```
